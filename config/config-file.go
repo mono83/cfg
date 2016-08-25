04fa285232934ac425b2e94fb562b390ec39c53c
Gotterdemarung <gotterdemarung@gmail.com>
2016-08-25 11:48:53 +0300

    file type auto determination

---
+++ config/config-file.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
+// AutoFind registers configuration source read from required file
+// File will searched in current folder, then in home folder and then in /etc/
+// File type will be determined from its extension
+func AutoFind(name, subfolderName string) error {
+	return def.AutoFind(name, subfolderName)
+}
+
+// AutoFind registers configuration source read from required file
+// File will searched in current folder, then in home folder and then in /etc/
+// File type will be determined from its extension
+func (c *Config) AutoFind(name, subfolderName string) error {
+	ext := filepath.Ext(name)
+	ext = strings.ToLower(ext)
+	switch ext {
+	case "yaml", "yml":
+		c.YAMLAutoFind(name, subfolderName)
+	case "json":
+		c.JSONAutoFind(name, subfolderName)
+	case "":
+		return fmt.Errorf("File %s has no extension and it's type cannot be detemined", name)
+	default:
+		return fmt.Errorf("Extension %s for file %s not supported", ext, name)
+	}
+
+	return nil
+}
